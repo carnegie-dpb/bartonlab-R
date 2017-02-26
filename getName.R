9cda3598982f5f8b3e63ecdce008affffade8236
Sam Hokin <shokin@ncgr.org>
2017-02-26 10:02:37 -0700

    Use the first returned value of genealiases if name=id.

--- getName.R
+++ getName.R
@@ -1,4 +1,5 @@
 library("RPostgreSQL")
+source("~/R/getConnection.R")
 
 ##
 ## Retrieve the gene's name from the given gene ID.
@@ -7,6 +8,11 @@ getName = function(id, host="localhost") {
     conn = getConnection(host=host)
     id = toupper(id)
     gene = dbGetQuery(conn, paste("SELECT * FROM genes WHERE id='",id,"'",sep=""))
+    name = gene$name[1]
+    if (name==id) {
+        aliases = dbGetQuery(conn, paste("SELECT * FROM public.genealiases WHERE id='",id,"'",sep=""))
+        if (length(aliases$name)>0) name=aliases$name[1]
+    }
     dbDisconnect(conn)
-    return(gene$name[1])
+    return(name)
 }
