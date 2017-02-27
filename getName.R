bc1027cc1f8b43139b57a25332b8049d81c1fe7a
Sam Hokin <shokin@ncgr.org>
2017-02-27 08:11:28 -0700

    Use first alias if name not present.

--- getName.R
+++ getName.R
@@ -7,11 +7,15 @@ source("~/R/getConnection.R")
 getName = function(id, host="localhost") {
     conn = getConnection(host=host)
     id = toupper(id)
+    name = id
     gene = dbGetQuery(conn, paste("SELECT * FROM genes WHERE id='",id,"'",sep=""))
-    name = gene$name[1]
-    if (name==id) {
+    if (length(gene$name)>0 && gene$name[1]!=id) {
+        name = gene$name[1]
+    } else {
         aliases = dbGetQuery(conn, paste("SELECT * FROM public.genealiases WHERE id='",id,"'",sep=""))
-        if (length(aliases$name)>0) name=aliases$name[1]
+        if (length(aliases$name)>0) {
+            name = aliases$name[1]
+        }
     }
     dbDisconnect(conn)
     return(name)
