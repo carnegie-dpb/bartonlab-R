58ef1d0c298847d0e039e795eb0e0fe4d4847752
Sam Hokin <shokin@ncgr.org>
2017-03-08 18:45:21 -0700

    Changed to allow an array of IDs input.

--- getName.R
+++ getName.R
@@ -8,13 +8,15 @@ getName = function(id, host="localhost") {
     conn = getConnection(host=host)
     id = toupper(id)
     name = id
-    gene = dbGetQuery(conn, paste("SELECT * FROM genes WHERE id='",id,"'",sep=""))
-    if (length(gene$name)>0 && gene$name[1]!=id) {
-        name = gene$name[1]
-    } else {
-        aliases = dbGetQuery(conn, paste("SELECT * FROM public.genealiases WHERE id='",id,"'",sep=""))
-        if (length(aliases$name)>0) {
-            name = aliases$name[1]
+    for (i in 1:length(id)) {
+        gene = dbGetQuery(conn, paste("SELECT * FROM genes WHERE id='",id[i],"'",sep=""))
+        if (length(gene$name)>0 && gene$name[1]!=id) {
+            name[i] = gene$name[1]
+        } else {
+            aliases = dbGetQuery(conn, paste("SELECT * FROM public.genealiases WHERE id='",id[i],"'",sep=""))
+            if (length(aliases$name)>0) {
+                name[i] = aliases$name[1]
+            }
         }
     }
     dbDisconnect(conn)
